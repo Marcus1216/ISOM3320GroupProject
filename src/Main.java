cf0b22954ae554bf5b7e60bfc980318b42261462
QryanQ <hiulonglee@gmail.com>
2025-03-19 17:16:05 +0800

    drafted for new Account class

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,26 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    public class Accounts {
+        int accountID;
+        String currencyType;
+        double balance;
+        //transaction history
+        public Accounts(int accountID, String currencyType, double balance) {
+            this.accountID = accountID;
+            this.currencyType = currencyType;
+            this.balance = balance;
+            //transaction history
+        }
+        public void displayAccountInfo(){
+            System.out.printf("The account ID is %d, the currency type is %s, and the balance is %.2d" ,
+                    this.accountID , this.currencyType , this.balance)
+        }
+        public void addToBalance(double amount){
+            this.balance += amount;
         }
     }
-}
+
+    public static void main(String[] args) {
+        }
+    }
